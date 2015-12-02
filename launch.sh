7511b93bcc6c29e6f0fcbbcb3b67a2f7f2369c0b
kazheng <kzheng8@hawk.iit.edu>
2015-12-02 00:17:11 -0600

    fixed and restored from older file

--- launch.sh
+++ launch.sh
@@ -4,6 +4,6 @@
 
 declare -a instanceArray
 
--mapfile -t instanceArray < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --iam-instance-profile Name=$7 --associate-public-ip --user-data file://../itmo444-env/install-env.sh --output table | grep INstanceID | sed "s/|//g" | -d ' ' | sed "s/InstanceID//g")
+-mapfile -t instanceArray < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --iam-instance-profile Name=$7 --associate-public-ip --user-data file://../itmo444-env/install-env.sh --output table | grep InstanceID | sed "s/|//g" | tr -d ' ' | sed "s/InstanceID//g")
 
 echo ${instanceArray[@]}
